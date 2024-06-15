96db9a8f8bbff8f4e32160e94dd65012f5cafb13
Kai_Dove <115669403+KaiDove1@users.noreply.github.com>
2024-06-15 19:47:50 -0400

    Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,25 +9,34 @@ const Sidebar = () => {
         <img src="path_to_logo" alt="Logo" />
       </div>
       <nav>
-        <ul>
-          <li><Link to="about" smooth={true} duration={500}>About</Link></li>
-          <li><Link to="education" smooth={true} duration={500}>Education</Link></li>
-          <li><Link to="experience" smooth={true} duration={500}>Experience</Link></li>
-        </ul>
+        <div className="main-links">
+          <ul>
+            <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+            <li><Link to="ideas" smooth={true} duration={500}>Ideas</Link></li>
+            <li><Link to="cv" smooth={true} duration={500}>CV</Link></li>
+          </ul>
+        </div>
       </nav>
-      <div className="dropdown">
-        <h3>Projects</h3>
-        <ul className="dropdown-content">
-          <li>View All</li>
-          <li>Project 1</li>
-          <li>Project 2</li>
+      <div className="interfaces">
+        <h3>HUMAN INTERFACE</h3>
+        <ul>
+          <li>Apple</li>
+          <li>GoodNotes</li>
+        </ul>
+        <h3>WEB INTERFACE</h3>
+        <ul>
+          <li>Berkeleytime</li>
+          <li>Marqeta</li>
+          <li>PropertyGuru</li>
         </ul>
       </div>
       <div className="contact">
         <h3>CONTACT</h3>
         <ul>
           <li><a href="mailto:ybr8ff@virginia.edu">Mail</a></li>
+          <li><a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
           <li><a href="https://www.linkedin.com/in/kai-dove-5b02b2243/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+          <li><a href="https://read.cv" target="_blank" rel="noopener noreferrer">read.cv</a></li>
         </ul>
       </div>
     </div>
