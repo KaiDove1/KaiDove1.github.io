40c4f303e17dfc40781f5705be3c540ddbc156d9
Kai_Dove <115669403+KaiDove1@users.noreply.github.com>
2024-06-19 02:18:20 -0400

    fixed main material

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
 import About from './components/About/About.jsx';
-import Education from './components/Education/Education.jsx';
-import Experience from './components/Experience/Experience.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import Projects from './components/Projects/Projects.jsx';
 
@@ -14,12 +12,6 @@ function App() {
       <div className="content">
         <section id="about">
           <About />
-        </section>
-        <section id="education">
-          <Education />
-        </section>
-        <section id="experience">
-          <Experience />
         </section>
         <section id="projects">
           <Projects />
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,29 +1,73 @@
 import React from 'react';
 import './About.css';
 
-const MainContent = () => {
+// import image1 from 'path_to_image_1';
+// import image2 from 'path_to_image_2';
+import uva_campus from '../../resources/sunset_grounds_ss_18.jpg';
+
+const About = () => {
   return (
-    <div className="main-content">
+    <div className="about-content">
       <div className="header">
         <h1>Kai Dove</h1>
         <h2>海 / カイ (Kai)</h2>
       </div>
-      <div className="info">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
-        </p>
-        <p>
-          Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem.
-        </p>
+      
+      <div className="bio-section">
+        <div className="bio-images">
+          <div className="image-box">
+            <img alt="My craft began here" />
+            <p className="caption">My craft began here</p>
+          </div>
+          <div className="image-box">
+            <img alt="Description 2" />
+            <p className="caption">Description 2</p>
+          </div>
+        </div>
+        <div className="bio-text">
+          <p>
+            I am a dedicated computer science student at the University of Virginia with a strong GPA of 3.92. My passion for solving complex problems has driven me to excel in coursework such as Data Structures, Algorithms, and Advanced Software Development. I’ve led and contributed to projects including a secure infrastructure whistleblower platform and a dynamic personal portfolio website. My practical experience includes a Software Engineer Internship at EOR.us, where I engineered a resume parser application, and a research assistant role where I implemented creative problem-solving exercises. As President of the Science Honor Society, I pioneered methodologies in image registration and statistical shape analysis. These experiences have equipped me with a robust skill set in programming and technical tools, preparing me for a promising career in the tech industry.
+          </p>
+        </div>
       </div>
-      <div className="images">
-        <img src="path_to_image_1" alt="Description 1" />
-        <img src="path_to_image_2" alt="Description 2" />
-        <img src="path_to_image_3" alt="Description 3" />
-        <img src="path_to_image_4" alt="Description 4" />
+
+      <div className="education">
+        <h2>Education</h2>
+        <div className="education-item">
+          <div className="education-text">
+            <h3>University of Virginia</h3>
+            <h4>College of Engineering</h4>
+            <p className='education-degree'>Bachelors of Science in Computer Science</p>
+            <p className="education-dates">2022 - 2026</p>
+          </div>
+          <div className="education-image">
+            <img src={uva_campus} alt="uva campus" />
+          </div>
+        </div>
+      </div>
+
+      <div className="things-i-like">
+        <h2>Things I Like to Do</h2>
+        <div className="things-section">
+          <div className="things-images">
+            <div className="image-box">
+              <img alt="Hiking in the mountains" />
+              <p className="caption">Hiking in the mountains</p>
+            </div>
+            <div className="image-box">
+              <img alt="Playing the guitar" />
+              <p className="caption">Playing the guitar</p>
+            </div>
+          </div>
+          <div className="things-text">
+            <p>
+              In my free time, I love engaging in a variety of activities that enrich my life and keep me balanced. One of my favorite pastimes is hiking in the mountains, where I can connect with nature and enjoy the serene beauty of the outdoors. I also enjoy playing the guitar, which allows me to express my creativity through music. These hobbies not only provide relaxation but also inspire me in my academic and professional pursuits.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default MainContent;
+export default About;
